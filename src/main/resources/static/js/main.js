e1c5147ce5a84bd6b2217acbbf2544d15aa0158d
ryzykowskia <ryzykowskia@edc.expert>
2020-07-27 14:26:36 +0200

    dodano menu stacji i wyswietlanie po kliknieciu

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,29 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        items: [],
+        tracks: [],
+    },
+    methods: {
+        ftracks(id) {
+            axios.get('http://localhost:8080/api/tracks/'+id)
+              .then(response => (
+                     this.tracks = response.data
+              ))
+        },
+        search(author) {
+                    axios.get('http://localhost:8080/api/search/'+author)
+                      .then(response => (
+                             this.tracks = response.data
+                      ))
+         }
+    },
+    mounted() {
+        axios.get('http://localhost:8080/api/stations')
+            .then(response => (
+                this.items = response.data
+            ))
+    }
+
+})
+
