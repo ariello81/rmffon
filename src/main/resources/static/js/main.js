ff6ce2cad85780d1f2fee19461c2792c929b82a8
ryzykowskia <ryzykowskia@edc.expert>
2020-07-28 15:35:17 +0200

    ukrywanie i pojawianie sie tabeli z wynikami

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,20 +2,26 @@ var app = new Vue({
     el: '#app',
     data: {
         items: [],
-        tracks: []
+        tracks: [],
+        isTable: false,
+        isHidden: true
     },
     methods: {
         ftracks(id) {
             axios.get('http://localhost:8080/api/tracks/'+id)
-              .then(response => (
+                .then(response => (
                      this.tracks = response.data
-              ))
+                ))
+                this.isTable = true
+                this.isHidden = false
         },
         search(author) {
-                    axios.get('http://localhost:8080/api/search/'+author)
-                      .then(response => (
-                             this.tracks = response.data
-                      ))
+            axios.get('http://localhost:8080/api/search/'+author)
+                .then(response => (
+                    this.tracks = response.data
+                 ))
+                 this.isTable = true
+                 this.isHidden = false
         }
     },
     mounted() {
