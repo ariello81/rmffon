59eb0a7e635a144e052db8870436e51efa666124
ryzykowskia <ryzykowskia@edc.expert>
2021-12-17 14:48:15 +0100

    dodano interface StationServiceClient

--- src/main/java/pl/ryzykowski/rmffon/service/client/StationServiceClient.java
+++ src/main/java/pl/ryzykowski/rmffon/service/client/StationServiceClient.java
@@ -1,4 +1,11 @@
 package pl.ryzykowski.rmffon.service.client;
 
+import pl.ryzykowski.rmffon.dto.StationDTO;
+
+import java.util.List;
+
 public interface StationServiceClient {
+
+    List<StationDTO> getStations();
+
 }
