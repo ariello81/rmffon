2d3a7843057d9b1ba095edec60db275e3de9cbc5
ariello81 <arielr@o2.pl>
2023-01-05 19:02:32 +0100

    podniesiono jave do 11, wylaczone scheduling

--- src/main/java/pl/ryzykowski/rmffon/RmffonSongsApplication.java
+++ src/main/java/pl/ryzykowski/rmffon/RmffonSongsApplication.java
@@ -2,10 +2,8 @@ package pl.ryzykowski.rmffon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class RmffonSongsApplication {
 
 	public static void main(String[] args) {
